7b5d36e65c6c1f8758d1c0f5a7d0b883d690938a
spewlee <suelee_1@hotmail.com>
2022-01-20 22:40:49 +0800

    Started making it mobile responsive
    Created a second .css file to work on the mobile responsiveness.  Having some issues with trying to disable Tilt.js on mobile but I'm sure I will get there. Happy with the progress that I have made today.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
-    $(".tablet").scroll(function() {
-        if($(".tablet").scrollTop()){
+    $(window).scroll(function() {
+        if($(window).scrollTop()){
             $("#sticky-id").addClass("white");
         }
         else {
             $("#sticky-id").removeClass("white");
         }
-    })
+    });
 
     $(".tablet").tilt( {
         glare: true,
@@ -15,11 +15,13 @@ $(document).ready(function() {
         perspective: 8000
     });
 
-
-
-
-
-
-
-
 });
+
+function responsiveNav() {
+    var x = document.getElementById("sticky-id");
+    if (x.className === "sticky") {
+      x.className += " responsive";
+    } else {
+      x.className = "sticky";
+    }
+  }
